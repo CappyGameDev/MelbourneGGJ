3133d8d4ae53da2e2c92baebc3a5631ec705d509
Deeon Burgan <dburgan@live.com>
2018-01-28 09:49:57 +1100

    Fixed light flickering  * put the script on light source directly instead of a manager  * can choose how long it flickers for

--- 2017 Melbourne GGJ/Assets/Scripts/LightingBehaviour.cs
+++ 2017 Melbourne GGJ/Assets/Scripts/LightingBehaviour.cs
@@ -2,63 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LightingBehaviour : MonoBehaviour {
+public class LightingBehaviour : MonoBehaviour
+{
 
-    [Header("StartRoomLights")]
-    public GameObject switchBoardLight;
-    public GameObject ceilingLight;
+    public bool flashing;
+    public float flashTimer;
+    public Light flashLight;
 
-    private float timer0;
-
-    private float timer;
-    private float decayRate;
-
-    public float minFlickerSpeed;
-    public float maxFlickerSpeed;
-
-    private bool routineStop;
-
-    Coroutine ceilingFlicker;
-
-    private bool on;
-
-	// Use this for initialization
-	void Start () {
-        timer = 50;
-        decayRate = 1;
-        timer0 = 1;
-        switchBoardLight.GetComponent<Light>().enabled = false;
-        ceilingLight.GetComponent<Light>().enabled = false;
-        minFlickerSpeed = 1;
-        maxFlickerSpeed = 50;
-        ceilingFlicker = StartCoroutine(Flashing());
+    void Awake()
+    {
+        flashLight = GetComponent<Light>();
     }
-	
-	// Update is called once per frame
-	void Update () {
 
-        if (routineStop == true)
-        {
-            StopCoroutine(ceilingFlicker);
-        }
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(flash());
+    }
 
-        if (timer0 == 1)
+    // Update is called once per frame
+    void Update()
+    {
+        flashTimer -= Time.deltaTime;
+        if(flashTimer <= 0)
         {
-            timer -= decayRate;
-            if (timer <= 0)
-            {
-                timer = 0;
-                timer0 = 2;
-            }
+            flashing = false;
+            flashLight.enabled = true;
         }
-        if (timer0 == 2)
+    }
+
+    IEnumerator flash()
+    {
+        while (flashing)
         {
-            ceilingLight.GetComponent<Light>().enabled = true;
-            yield return new WaitForSeconds(Random.Range(minFlickerSpeed, maxFlickerSpeed));
+            yield return new WaitForSeconds(Random.Range(0.15f, 0.5f));
+            flashLight.enabled = !flashLight.enabled;
+
         }
-        if (timer0 == 3)
-        {
-        
-        }
-	}
+    }
 }
